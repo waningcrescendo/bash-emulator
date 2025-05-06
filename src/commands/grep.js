304690c266ab2a879a2f3322b0cdc3af83a79bff
Raphaela Parreno <122368672+waningcrescendo@users.noreply.github.com>
2025-05-06 16:57:30 +0200

    grep command with some flags

---
+++ src/commands/grep.js
@@ -0,0 +1,130 @@
+var lineNumber = require("../utils/lineNumber");
+
+function grep(env, args) {
+  // Ignore command name
+  args.shift();
+
+  var exitCode = 0;
+  var showNumbers = false;
+  var invertMatch = false;
+  var ignoreCase = false;
+  var countOnly = false;
+
+  // flag extraction
+  var flags = [];
+  while (args[0] && args[0].startsWith("-")) {
+    var flag = args.shift();
+    for (var i = 1; i < flag.length; i++) {
+      flags.push("-" + flag[i]);
+    }
+  }
+
+  // flag interpretation
+  flags.forEach(function (f) {
+    if (f === "-n") {
+      showNumbers = true;
+      console.log("showNumbers is true");
+    }
+    if (f === "-v") {
+      invertMatch = true;
+      console.log("invertmatch is true");
+    }
+    if (f === "-i") {
+      ignoreCase = true;
+      console.log("ignorecase is true");
+    }
+    if (f === "-c") {
+      countOnly = true;
+      console.log("countonly is true");
+    }
+  });
+
+  var rawPattern = args.shift();
+  if (!rawPattern) {
+    env.error("need pattern for grep");
+    return env.exit(2);
+  }
+
+  // pattern
+  var pattern = rawPattern.replace(/^['"]|['"]$/g, "");
+  var reFlags = ignoreCase ? "i" : "";
+  var re = new RegExp(pattern, reFlags);
+  console.log("pattern ", pattern);
+
+  function processLines(lines, prefix) {
+    console.log("process lines ", lines);
+    var matchCount = 0;
+    lines.forEach(function (line, idx) {
+      var isMatch = re.test(line);
+      if (invertMatch) {
+        isMatch = !isMatch;
+      }
+      if (isMatch) {
+        matchCount++;
+        if (!countOnly) {
+          var out = line;
+          if (showNumbers) {
+            out = addLineNumber.addLineNumber(6, idx + 1, out);
+          }
+          if (prefix) {
+            out = prefix + ":" + out;
+          }
+          env.output(out + "\n");
+        }
+      }
+    });
+    return matchCount;
+  }
+
+  // if no file, read stdin
+  if (args.length === 0) {
+    console.log("NO FILE");
+    var buffer = [];
+    return {
+      input: function (chunk) {
+        buffer = buffer.concat(chunk.split("\n"));
+        console.log("BUFFER ", buffer);
+      },
+      close: function () {
+        var total = processLines(buffer, null);
+        console.log("total ", total);
+        if (countOnly) {
+          env.output(total + "\n");
+        }
+        console.log("exit ", exitCode);
+        env.exit(exitCode);
+      },
+    };
+  }
+
+  // if file present
+  Promise.all(
+    args.map(function (path) {
+      return env.system.read(path).then(
+        function (content) {
+          console.log("content ", content);
+          return content.split("\n");
+        },
+        function (err) {
+          exitCode = 1;
+          env.error("grep: " + err + "\n");
+          return [];
+        }
+      );
+    })
+  ).then(function (allLinesArrays) {
+    var multiple = args.length > 1;
+    var grandTotal = 0;
+
+    allLinesArrays.forEach(function (lines, idx) {
+      var prefix = multiple ? args[idx] : null;
+      console.log("prefix ", prefix);
+      grandTotal += processLines(lines, prefix);
+    });
+    if (countOnly) {
+      env.output(grandTotal + "\n");
+    }
+    env.exit(exitCode);
+  });
+}
+module.exports = grep;
--- src/commands/index.js
+++ src/commands/index.js
@@ -1,15 +1,16 @@
 var commands = {
-  cat: require('./cat'),
-  cd: require('./cd'),
-  cp: require('./cp'),
-  history: require('./history'),
-  ls: require('./ls'),
-  mkdir: require('./mkdir'),
-  mv: require('./mv'),
-  pwd: require('./pwd'),
-  rm: require('./rm'),
-  rmdir: require('./rmdir'),
-  touch: require('./touch')
-}
+  cat: require("./cat"),
+  grep: require("./grep"),
+  cd: require("./cd"),
+  cp: require("./cp"),
+  history: require("./history"),
+  ls: require("./ls"),
+  mkdir: require("./mkdir"),
+  mv: require("./mv"),
+  pwd: require("./pwd"),
+  rm: require("./rm"),
+  rmdir: require("./rmdir"),
+  touch: require("./touch"),
+};
 
-module.exports = commands
+module.exports = commands;
