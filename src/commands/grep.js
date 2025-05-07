a3a8555ce0152e5d87349f241dfd3e0ec872394a
Raphaela Parreno <122368672+waningcrescendo@users.noreply.github.com>
2025-05-07 11:43:43 +0200

    format standard

--- src/commands/grep.js
+++ src/commands/grep.js
@@ -1,100 +1,101 @@
-var lineNumber = require("../utils/lineNumber");
+var lineNumber = require('../utils/lineNumber')
 
-function grep(env, args) {
+function grep (env, args) {
   // Ignore command name
-  args.shift();
+  args.shift()
 
-  var exitCode = 0;
-  var showNumbers = false;
-  var invertMatch = false;
-  var ignoreCase = false;
-  var countOnly = false;
+  var exitCode = 0
+  var showNumbers = false
+  var invertMatch = false
+  var ignoreCase = false
+  var countOnly = false
 
   // flag extraction
-  var flags = [];
-  while (args[0] && args[0].startsWith("-")) {
-    var flag = args.shift();
+  var flags = []
+  while (args[0] && args[0].startsWith('-')) {
+    var flag = args.shift()
     for (var i = 1; i < flag.length; i++) {
-      flags.push("-" + flag[i]);
+      flags.push('-' + flag[i])
     }
   }
 
   // flag interpretation
   flags.forEach(function (f) {
-    if (f === "-n") {
-      showNumbers = true;
-      console.log("showNumbers is true");
+    if (f === '-n') {
+      showNumbers = true
+      console.log('showNumbers is true')
     }
-    if (f === "-v") {
-      invertMatch = true;
-      console.log("invertmatch is true");
+    if (f === '-v') {
+      invertMatch = true
+      console.log('invertmatch is true')
     }
-    if (f === "-i") {
-      ignoreCase = true;
-      console.log("ignorecase is true");
+    if (f === '-i') {
+      ignoreCase = true
+      console.log('ignorecase is true')
     }
-    if (f === "-c") {
-      countOnly = true;
-      console.log("countonly is true");
+    if (f === '-c') {
+      countOnly = true
+      console.log('countonly is true')
     }
-  });
+  })
 
-  var rawPattern = args.shift();
+  var rawPattern = args.shift()
   if (!rawPattern) {
-    env.error("need pattern for grep");
-    return env.exit(2);
+    env.error('need pattern for grep')
+    return env.exit(2)
   }
 
   // pattern
-  var pattern = rawPattern.replace(/^['"]|['"]$/g, "");
-  var reFlags = ignoreCase ? "i" : "";
-  var re = new RegExp(pattern, reFlags);
-  console.log("pattern ", pattern);
+  var pattern = rawPattern.replace(/^['"]|['"]$/g, '')
+  var reFlags = ignoreCase ? 'i' : ''
+  var re = new RegExp(pattern, reFlags)
+  console.log('pattern ', pattern)
 
-  function processLines(lines, prefix) {
-    console.log("process lines ", lines);
-    var matchCount = 0;
+  function processLines (lines, prefix) {
+    if (lines.length && lines[lines.length - 1] === '') {
+      lines.pop()
+    }
+    var matchCount = 0
     lines.forEach(function (line, idx) {
-      var isMatch = re.test(line);
+      var isMatch = re.test(line)
       if (invertMatch) {
-        isMatch = !isMatch;
+        isMatch = !isMatch
       }
       if (isMatch) {
-        matchCount++;
+        matchCount++
         if (!countOnly) {
-          var out = line;
+          var out = line
           if (showNumbers) {
-            out = addLineNumber.addLineNumber(6, idx + 1, out);
+            out = lineNumber(6, idx + 1, out)
           }
           if (prefix) {
-            out = prefix + ":" + out;
+            out = prefix + ':' + out
           }
-          env.output(out + "\n");
+          console.log()
+          env.output(out + '\n')
         }
       }
-    });
-    return matchCount;
+    })
+    return matchCount
   }
 
   // if no file, read stdin
   if (args.length === 0) {
-    console.log("NO FILE");
-    var buffer = [];
+    console.log('NO FILE')
+    var buffer = []
     return {
       input: function (chunk) {
-        buffer = buffer.concat(chunk.split("\n"));
-        console.log("BUFFER ", buffer);
+        buffer = buffer.concat(chunk.split('\n'))
       },
       close: function () {
-        var total = processLines(buffer, null);
-        console.log("total ", total);
+        var total = processLines(buffer, null)
         if (countOnly) {
-          env.output(total + "\n");
+          env.output(total + '\n')
         }
-        console.log("exit ", exitCode);
-        env.exit(exitCode);
-      },
-    };
+        console.log('exit ', exitCode)
+        env.exit(exitCode)
+      }
+    }
   }
 
   // if file present
@@ -102,29 +103,28 @@ function grep(env, args) {
     args.map(function (path) {
       return env.system.read(path).then(
         function (content) {
-          console.log("content ", content);
-          return content.split("\n");
+          return content.split('\n')
         },
         function (err) {
-          exitCode = 1;
-          env.error("grep: " + err + "\n");
-          return [];
+          exitCode = 1
+          env.error('grep: ' + err + '\n')
+          return []
         }
-      );
+      )
     })
   ).then(function (allLinesArrays) {
-    var multiple = args.length > 1;
-    var grandTotal = 0;
+    var multiple = args.length > 1
+    var grandTotal = 0
 
     allLinesArrays.forEach(function (lines, idx) {
-      var prefix = multiple ? args[idx] : null;
-      console.log("prefix ", prefix);
-      grandTotal += processLines(lines, prefix);
-    });
+      var prefix = multiple ? args[idx] : null
+      grandTotal += processLines(lines, prefix)
+    })
     if (countOnly) {
-      env.output(grandTotal + "\n");
+      console.log('COUNT ONLY')
+      env.output(grandTotal + '\n')
     }
-    env.exit(exitCode);
-  });
+    env.exit(exitCode)
+  })
 }
-module.exports = grep;
+module.exports = grep
